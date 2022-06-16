3331036fd258d8b62b310a66027c8182a779e7a7
Brian Shelby <bsshel@yahoo.com>
2022-06-16 15:33:13 -0500

    Add Week 2 HW and Readme update

---
+++ HwWk2.js
@@ -0,0 +1,50 @@
+// let pizzaPlace = "God Fathers Pizza";
+// let numberOfToppings = 4;
+
+// console.log(typeof pizzaPlace);
+// console.log(typeof numberOfToppings);
+// console.log(`${pizzaPlace} is the best when you add ${numberOfToppings} toppings.`)
+
+// Printed to Chrome console
+
+// let pizzaPlace = "God Fathers Pizza";
+// let numberOfToppings = 4;
+
+// console.log(typeof pizzaPlace);
+// console.log(typeof numberOfToppings);
+// console.log(`${pizzaPlace} is the best when you add ${numberOfToppings} toppings.`)
+// VM67:4 string
+// VM67:5 number
+// VM67:6 God Fathers Pizza is the best when you add 4 toppings.
+
+// let toppings = 10;
+
+if (toppings < 10) {
+  console.log("Quality, not quantity.")
+} else {
+  console.log("A whole lot of toppings")
+}
+
+// create a variable outside of the while loop
+// inside the while loop, we keep track of it and re-assign it until the condition becomes false
+// let x = 1;
+
+// while x is less than or equal to 10,
+while (x <= 10) {
+  // print x
+  console.log(x);
+}
+  // increment by one and re-assign x
+  // if we forgot this step, x <= 10 always evaluates to true and we create an infinite loop
+//   x++;
+// }
+
+// set i equal to 1
+// while i is less than or equal to 10, run the code inside the scope
+// after running the code, increase i by 1 and try the loop again
+
+// **Bonus Challenge**
+for (let i = 1; i <= 10; i++) {
+  if (i%2 !=1)
+  console.log(i);
+  }

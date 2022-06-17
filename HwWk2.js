e241bfe8e941a7b144ef8bb19a45d74be0cc2868
Brian Shelby <bsshel@yahoo.com>
2022-06-17 10:07:07 -0500

    Update HW Week2

--- HwWk2.js
+++ HwWk2.js
@@ -1,23 +1,26 @@
-// let pizzaPlace = "God Fathers Pizza";
-// let numberOfToppings = 4;
+// 4. Create two variables
+// 5. Reference the name of your pizza place and the number of toppings
 
-// console.log(typeof pizzaPlace);
-// console.log(typeof numberOfToppings);
-// console.log(`${pizzaPlace} is the best when you add ${numberOfToppings} toppings.`)
+let pizzaPlace = "God Fathers Pizza";
+let numberOfToppings = 4;
 
-// Printed to Chrome console
+// 6. Print the variables and their types
 
-// let pizzaPlace = "God Fathers Pizza";
-// let numberOfToppings = 4;
+console.log(typeof pizzaPlace);
+console.log(typeof numberOfToppings);
+VM67:4 string
+VM67:5 number
 
-// console.log(typeof pizzaPlace);
-// console.log(typeof numberOfToppings);
-// console.log(`${pizzaPlace} is the best when you add ${numberOfToppings} toppings.`)
-// VM67:4 string
-// VM67:5 number
-// VM67:6 God Fathers Pizza is the best when you add 4 toppings.
+// 7. Print a template literal that uses both variables to construct a short sentence
 
-// let toppings = 10;
+console.log(`${pizzaPlace} is the best when you add ${numberOfToppings} toppings.`)
+VM67:6 God Fathers Pizza is the best when you add 4 toppings.
+
+// 8. Construct an if statement that prints "Quality, not quantity."
+//    if you offer less than 10 toppings, or "A whole lot of pizza."
+//    if you offer 10 or more toppings.
+
+let toppings = 10;
 
 if (toppings < 10) {
   console.log("Quality, not quantity.")
@@ -25,26 +28,8 @@ if (toppings < 10) {
   console.log("A whole lot of toppings")
 }
 
-// create a variable outside of the while loop
-// inside the while loop, we keep track of it and re-assign it until the condition becomes false
-// let x = 1;
-
-// while x is less than or equal to 10,
-while (x <= 10) {
-  // print x
-  console.log(x);
-}
-  // increment by one and re-assign x
-  // if we forgot this step, x <= 10 always evaluates to true and we create an infinite loop
-//   x++;
-// }
-
-// set i equal to 1
-// while i is less than or equal to 10, run the code inside the scope
-// after running the code, increase i by 1 and try the loop again
-
-// **Bonus Challenge**
-for (let i = 1; i <= 10; i++) {
-  if (i%2 !=1)
-  console.log(i);
-  }
+// Bonus Challenge
+for (let i = 1; i <= 10; i++)
+  if (i%2 !=1) {
+  console.log(i)
+  };
